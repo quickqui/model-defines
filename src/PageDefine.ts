b7477a4daebc655f58ac41270f54f7aaf0c56d20
nielinjie <nielinjie@gmail.com>
2019-11-05 10:19:51 +0800

    init

---
+++ src/PageDefine.ts
@@ -0,0 +1,18 @@
+import { Model, ValidateError } from "@quick-qui/model-core";
+
+const define = {
+    validatePiece(piece: any):ValidateError[]{
+        return []
+    },
+    merge(model: Model, piece: any): Model {
+        const _model = model as any;
+        return {
+            ..._model,
+            pages: (_model.pages || []).concat(piece.pages || [])
+        } as Model
+    },
+
+    validateAfterMerge(model: Model): ValidateError[] { return [] },
+    validateAfterWeave(model: Model): ValidateError[] { return [] }
+}
+export default define
