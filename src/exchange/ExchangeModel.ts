3f9484c6f514aa165def5b08b200939ac6d68717
nielinjie <nielinjie@gmail.com>
2020-01-27 12:23:15 +0800

    小review

--- src/exchange/ExchangeModel.ts
+++ src/exchange/ExchangeModel.ts
@@ -26,6 +26,7 @@ export interface Exchange extends WithNamespace, WithAnnotations {
   from: string;
   to: string; //externals, front, back, database,
   protocol: string; //dp, rest, grapghql
+  //TODO exchange的extension是用于实现的，不合适，考虑放到implementation模型。
   extension?: string;
   resources: string[];
   order?: number;
