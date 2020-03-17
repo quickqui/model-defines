7c792f8583890557ec9964492d5ad990a7b47213
nielinjie <nielinjie@gmail.com>
2020-03-17 20:25:37 +0800

    global bug

--- src/implementation/ImplementationGlobal.ts
+++ src/implementation/ImplementationGlobal.ts
@@ -1,7 +1,22 @@
 import { StringKeyObject } from "../BaseDefine";
+import * as path from "path";
 //TODO 不知道是否合适
 //TODO 不知道在复杂require环境下是否能在runtime范围里面唯一。
 //TODO Implementation是否也就是一个runtime，还没确定。
 //TODO 目前来看，一个implementation等于一个runtime，后续需要review。
-export const implementationGlobal :StringKeyObject = {}
-export const runtimeGlobal :StringKeyObject = implementationGlobal
+const implementationGlobalKey = "implementationGlobal";
+
+if (globalThis != undefined) {
+  if (globalThis[implementationGlobalKey]) {
+  } else {
+    globalThis[implementationGlobalKey] = {
+      createAt: path.resolve(__dirname)
+    };
+  }
+} else {
+  throw new Error("global not supported");
+}
+
+export const implementationGlobal: StringKeyObject =
+  globalThis[implementationGlobalKey];
+export const runtimeGlobal: StringKeyObject = implementationGlobal;
